982343e228719405ab21475b18186a12faec6504
Suchitra Edussuriya <sse_p@hotmail.com>
2013-10-11 04:17:24 -0700

    oop blackjack

---
+++ blackjack_oop.rb
@@ -0,0 +1,277 @@
+class Card
+  attr_accessor :suit, :value
+
+  def initialize(s,v)
+    @suit = s
+    @value = v
+  end
+end
+
+class Deck
+  attr_accessor :deck
+  
+  def initialise
+    @deck = []
+    ['Hearts', 'Spades', 'Clubs', 'Diamonds'].each do |suit|
+      ['2', '3', '4', '5', '6', '7', '8', '9', '10','Ace', 'King', 'Queen', 'Jack'].each do |value|
+        @deck << Card.new(suit, value)
+        puts "Test"
+      end
+    end
+    scramble!
+  end
+  
+  def scramble!
+    deck.shuffle!
+  end
+
+  def deal_one 
+    deck.pop
+  end
+
+  def size
+    deck.size
+  end
+end
+
+module Hand
+  def show_hand
+    puts "------#{name}'s Hand ------"
+    cards.each do|card|
+      puts "=> #{card}"
+    end
+
+    if total[1] != 0
+      puts "=> Your two totals are: #{total[0]} and #{total[1]}"
+    else
+      puts "=> Your total is: #{total[0]}"
+      player.best_total = total[0]
+    end
+  end
+
+  def total
+    face_values = cards.map{|card| card.face_value}
+
+    sum1 = 0
+    sum2 = 0
+
+    face_values.each do |val|
+      if val == "Ace"
+        sum2 += 1
+      else
+        sum1 += (val.to_i == 0 ? 10 : val.to_i)    
+      end
+    end
+
+    if face_values.include? "Ace"
+       sum2 = sum1 + 10
+    end
+
+    return sum1, sum2
+  end
+
+  def add_card(new_card)
+    cards << new_card
+  end
+
+  def busted
+   total[0] > Blackjack::BLACKJACK_AMOUNT && total[1] > BLACKJACK_AMOUNT
+  end
+end
+
+class Player
+  include Hand
+
+  attr_accessor :name, :cards, :best_total
+
+  def initialize(n)
+    @name = n
+    @cards = []
+    @best_total = 0
+  end
+
+  def show_flop
+    show_hand
+  end
+end
+
+class Dealer
+  include Hand
+
+  attr_accessor :name, :cards, :best_total
+
+  def initialize
+    @name = "Dealer"
+    @cards = []
+    @best_total = 0
+  end
+  
+  def show_flop
+    puts "----Dealer's Hand ------"
+    puts "=> First card is hidden"
+    puts "=> Second card is #{cards[1]}"
+  end
+
+end
+
+class Blackjack
+  
+  attr_accessor :player, :dealer, :gdeck
+
+  BLACKJACK_AMOUNT = 21
+  DEALER_HIT_MIN = 17
+  
+  def initialize
+    @player = Player.new("Player1")
+    @dealer = Dealer.new
+    @gdeck = Deck.new
+  end
+
+  def set_player_name
+    puts "What's your name?"
+    player.name = gets.chomp.capitalize
+  end
+
+
+
+
+  def deal_cards
+    player.add_card(gdeck.deal_one)
+    dealer.add_card(gdeck.deal_one)
+    player.add_card(gdeck.deal_one)
+    dealer.add_card(gdeck.deal_one)
+  end
+
+  def show_flop
+    player.show_flop
+    dealer.show_flop
+  end
+  
+  def blackjack_or_bust?(player_or_dealer)
+    if player_or_dealer.total[0] == BLACKJACK_AMOUNT || player_or_dealer.total[1] == BLACKJACK_AMOUNT
+      if player_or_dealer.is_a?(Dealer)
+        puts "Sorry, dealer hit blackjack. #{player_name} loses."
+      else
+        puts "Congratulations, you hit blackjack! #{player.name} wins!"
+      end
+      play_again?
+    elsif player_or_dealer.is_busted?
+      if player_or_dealer.is_a?(Dealer)
+        puts "Congratulations, dealer busted. #{player.name} wins!"
+      else 
+        puts "Sorry, #{player.name} busted. #{player.name} loses."
+      end
+      play_again?
+
+    end
+  end
+
+  def say
+    puts 'Would you like to hit or stay?'
+    answer = gets.chomp.downcase
+    return answer
+  end
+
+  def player_goes
+    blackjack_or_bust?(player)
+    say
+    while say == 'hit'
+      new_card = gdeck.deal_one
+      puts "Dealing card to #{player.name}: #{new_card}"
+      player.add_card(new_card)
+      if player.total[1] != 0
+        puts "=> Your two totals are: #{player.total[0]} and #{player.total[1]}"
+        if (player.total[0] < player.total[1]) && (player.total[1] < 21)
+          player.best_total = player.total[1]
+        else
+          player.best_total = player.total[0]
+        end
+      else
+        puts "=> Your total is: #{player.total[0]}"
+        player.best_total = player.total[0]
+      end
+      blackjack_or_bust?(player)
+      say
+    end
+    
+    puts "#{player.name} stays at #{player.best_total}."
+
+  end
+
+  def dealer_goes
+    blackjack_or_bust?(dealer)
+    if dealer.total[1] != 0
+        puts "=> House totals are: #{dealer.total[0]} and #{dealer.total[1]}"
+        if (dealer.total[1] < 21)
+          dealer.best_total = dealer.total[1]
+        else
+          dealer.best_total = dealer.total[0]
+        end 
+      else
+        puts "=> House total is: #{dealer.total[0]}"
+        dealer.best_total = dealer.total[0]
+      end
+    
+    while dealer.best_total < DEALER_HIT_MIN
+      new_card = gdeck.deal_one
+      puts "Dealing car to dealer: #{new_card}"
+      dealer.add_card(new_card)
+      if dealer.total[1] != 0
+        puts "=> House totals are: #{dealer.total[0]} and #{dealer.total[1]}"
+        if (dealer.total[1] < 21)
+          dealer.best_total = dealer.total[1]
+        else
+          dealer.best_total = dealer.total[0]
+        end 
+      else
+        puts "=> House total is: #{dealer.total[0]}"
+        dealer.best_total = dealer.total[0]
+      end
+      blackjack_or_bust?(dealer)
+
+    end 
+
+    puts "Dealer stays at #{dealer.best_total}"
+
+  end
+
+  def who_won?
+    if dealer.best_total == player.best_total
+      puts "It's a push"
+    elsif dealer.best_total > 21
+      puts "#{player.name} wins!"
+    elsif dealer.best_total < player.best_total
+      puts "#{player.name} wins!"
+    elsif (dealer.best_total <= 21) && (dealer.best_total > player.best_total)
+      puts 'House wins'
+    end
+  end
+  
+  def play_again?
+    puts "#{name} would you like to play again? "
+    if gets.chomp.downcase == 'yes'
+      puts "Starting new game"
+      puts " "
+      gdeck = Deck.new
+      player.cards = []
+      dealer.cards = []
+      start
+    else
+      puts "Goodbye"
+      exit
+    end
+  end
+
+  def run
+    set_player_name
+    deal_cards
+    show_flop
+    player_goes
+    dealer_goes
+    who_won?
+  end
+end
+
+
+Blackjack.new.run
+
